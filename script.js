f8feeb5599bc0f4b281679d14292f19c5fbee145
medoed <ravbetsky@gmail.com>
2014-11-16 17:59:06 +0300

    added winner by column logic

--- script.js
+++ script.js
@@ -5,9 +5,9 @@
 			playerFlag = true; // if hui then true, else pizda
 			arrayLength = mainArray.length;
 
+		trackChanges(0,0,'x');
+		trackChanges(1,0,'x');
 		trackChanges(2,0,'x');
-		trackChanges(2,1,'x');
-		trackChanges(2,2,'x');
 		alert(checkWinner());
 		alert(mainArray);
 
@@ -29,16 +29,24 @@
 		function checkWinner() {
 			var i = 0,
 				j = 0,
-				winFlagByRow = false;
-				//winFlagByColumn = false;
+				heplerFlagArray = [],
+				winFlagByRow = true,
+				winFlagByColumn = true;
 				//winFlag3 = true;
 				finalWinFlag = false;
 
 			for (i; i < arrayLength; i++) {
 				for (j = 0; j < arrayLength; j++) {
-					if (mainArray[i][j] && (mainArray[i][j+1] != undefined)) {
-						if ((mainArray[i][j] == mainArray[i][j+1])) {
-							winFlagByRow = true;
+					// Check if player win by row values
+					if (mainArray[i][j] && mainArray[i][j+1] != undefined) {
+						if (mainArray[i][j] !== mainArray[i][j+1]) {
+							winFlagByRow = false;
+						}
+					}
+					// Check if player win by column values
+					if (i>=1 && heplerFlagArray.indexOf(j) === -1) {
+						if (mainArray[i][j] !== mainArray[i-1][j] || mainArray[i][j] === 0) {
+							heplerFlagArray.push(j);
 						}
 					}
 				}
@@ -46,8 +54,12 @@
 					finalWinFlag = true;
 				}
 			}
+
+			if (heplerFlagArray.length !== arrayLength) {
+				finalWinFlag = true;
+			}
 			
-			return finalWinFlag ? 'Congrats!!! Somebody has won!' : 'Sorry you not a winner!';
+			return finalWinFlag ? 'Congrats!!! Somebody has won!' : 'Sorry you are not a winner!';
 		}
 	}
 
