31a265f5a2cb0c38922c6b7f3b716003a7287e72
medoed <ravbetsky@gmail.com>
2014-11-17 01:30:10 +0300

    added main game logic: checkwinner func

--- script.js
+++ script.js
@@ -5,12 +5,6 @@
 			playerFlag = true; // if hui then true, else pizda
 			arrayLength = mainArray.length;
 
-		trackChanges(0,0,'x');
-		trackChanges(1,0,'x');
-		trackChanges(2,0,'x');
-		alert(checkWinner());
-		alert(mainArray);
-
 		function trackChanges(posX, posY, pSign) {
 			var playerSign = playerFlag ? 'x' : 'o';
 
@@ -29,17 +23,22 @@
 		function checkWinner() {
 			var i = 0,
 				j = 0,
+				helperDiagonalValue1 = mainArray[0][0],
+				helperDiagonalDesc = arrayLength-1,
+				helperDiagonalValue2 = mainArray[0][helperDiagonalDesc],
 				heplerFlagArray = [],
 				winFlagByRow = true,
-				winFlagByColumn = true;
-				//winFlag3 = true;
+				winFlagByColumn = true,
+				winFlagByDiagonal1 = true,
+				winFlagByDiagonal2 = true,
 				finalWinFlag = false;
 
 			for (i; i < arrayLength; i++) {
 				for (j = 0; j < arrayLength; j++) {
+					winFlagByRow = true;
 					// Check if player win by row values
-					if (mainArray[i][j] && mainArray[i][j+1] != undefined) {
-						if (mainArray[i][j] !== mainArray[i][j+1]) {
+					if (mainArray[i][j-1] != undefined) {
+						if (mainArray[i][j] !== mainArray[i][j-1] || mainArray[i][j] === 0) {
 							winFlagByRow = false;
 						}
 					}
@@ -49,13 +48,28 @@
 							heplerFlagArray.push(j);
 						}
 					}
+					// Check if player win by diagonal
+					if (i==j) {
+						if (mainArray[i][j] !== helperDiagonalValue1 || mainArray[i][j] === 0) {
+							winFlagByDiagonal1 = false;
+						}
+						helperDiagonalValue1 = mainArray[i][j];
+					}
+					if (j==helperDiagonalDesc) {
+						console.log(mainArray[i][j] + '['+i+']' + '['+j+']' +':' + helperDiagonalDesc + ':' + helperDiagonalValue2);
+						if (mainArray[i][j] !== helperDiagonalValue2 || mainArray[i][j] === 0) {
+							winFlagByDiagonal2 = false;
+						}
+						helperDiagonalValue2 = mainArray[i][j];
+						helperDiagonalDesc--;
+					}
 				}
 				if (winFlagByRow) {
 					finalWinFlag = true;
 				}
 			}
 
-			if (heplerFlagArray.length !== arrayLength) {
+			if (heplerFlagArray.length !== arrayLength || winFlagByDiagonal1 || winFlagByDiagonal2) {
 				finalWinFlag = true;
 			}
 			
